1332f9cd4160fbf23737b54e41c7d7ca009a1547
mbaremedsalem <mbaremedsalemmbare@gmail.com>
2024-05-23 05:44:44 -0700

    add new

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 sudo apt-get install python3-dev
 pip install pygraphviz
-pip install -r requirements.txt
 pip3 install -r requirements.txt
+pip install -r requirements.txt
 python manage.py collectstatic
